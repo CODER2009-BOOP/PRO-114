e9ebe817aaa61fc6e0ef5093c9623ac7a7242acb
CODER2009-BOOP <70509810+CODER2009-BOOP@users.noreply.github.com>
2022-07-29 06:08:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("main.csv")
+
+TOEFL_Score = df["TOEFL Score"].tolist()
+Chances_of_admit = df["Chance of Admit "].tolist()
+
+fig = px.scatter(x=TOEFL_Score, y=Chances_of_admit)
+fig.show()
+
+m = 0.01
+c = -2.5
+y = []
+for x in TOEFL_Score:
+  y_value = m*x + c
+  y.append(y_value)
+
+#Plotting the points
+fig = px.scatter(x=TOEFL_Score, y=Chances_of_admit)
+fig.update_layout(shapes=[
+    dict(
+      type= 'line',
+      y0= min(y), y1= max(y),
+      x0= min(TOEFL_Score), x1= max(TOEFL_Score)
+    )
+])
+fig.show()
